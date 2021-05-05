69c752a6df6a7c9d386bad4c09910ba5fefc8b02
d4v1dy3 <daveborto@icloud.com>
2021-05-05 14:01:24 +0200

    Interfaccia
    Interfaccia corretta

--- ProgettoInformatica/src/View/Interfaccia.java
+++ ProgettoInformatica/src/View/Interfaccia.java
@@ -1,12 +1,43 @@
-/**
- * 
- */
 package View;
 
-/**
- * @author davidebortoletto
- *
- */
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
 public class Interfaccia {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Interfaccia window = new Interfaccia();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Interfaccia() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 
 }
